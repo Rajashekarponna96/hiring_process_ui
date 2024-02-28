b9485f0913852f10a0d35c4b2aec7530666666b5
rajashekarponna96 <rajashekar.ponna96@gmail.com>
2024-02-28 11:47:36 +0530

    added candidate feilds

--- src/app/demo/components/dashboards/candidate/candidate.component.ts
+++ src/app/demo/components/dashboards/candidate/candidate.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Candidate } from '../../model/candidate';
 @Component({
-  selector: 'app-candidate',
-  templateUrl: './candidate.component.html',
-  styleUrls: ['./candidate.component.scss']
+    selector: 'app-candidate',
+    templateUrl: './candidate.component.html',
+    styleUrls: ['./candidate.component.scss'],
 })
-export class CandidateComponent {
+export class CandidateComponent implements OnInit {
+    skillOptions: any[] = [
+        { name: 'Java' },
+        { name: 'Python' },
+        { name: 'C++' },
+    ];
+    selectedSource: string | null = null;
 
+    candidate!: FormGroup;
+
+    isSubmitted: boolean = false;
+    selectedSkills: any[] = []; // Assuming this array holds the selected skills
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.initializeForm();
+    }
+
+    initializeForm(): void {
+        this.candidate = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            gmail: ['', [Validators.required, Validators.email]],
+            mobile: ['', Validators.required],
+            source: ['', Validators.required],
+            availableTo: ['', Validators.required],
+            currentLocation: ['', Validators.required],
+            preferredLocation: ['', Validators.required],
+            expectedSalary: ['', Validators.required],
+            gender: ['', Validators.required],
+            dob: ['', Validators.required],
+            candidateOwner: ['', Validators.required],
+            course: [''],
+            branch: [''],
+            startDate: [''],
+            endDate: [''],
+            university: [''],
+            location: [''],
+        });
+    }
+
+    onSave(): void {
+        console.log('Save button clicked');
+    }
+
+    onSubmit(): void {
+        this.isSubmitted = true;
+        if (this.candidate.valid) {
+            console.log('Form submitted successfully!', this.candidate.value);
+
+            const sourceControl = this.candidate.get('source');
+            if (sourceControl && sourceControl.value) {
+                this.selectedSource = sourceControl.value;
+                console.log('Selected Source:', this.selectedSource);
+            } else {
+                console.log('Source is not selected or form control is null.');
+            }
+
+            this.candidate.reset();
+        } else {
+            console.log('Form has validation errors.');
+        }
+    }
 }
--- src/app/demo/components/dashboards/candidate/candidate.module.ts
+++ src/app/demo/components/dashboards/candidate/candidate.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CandidateRoutingModule } from './candidate-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AutoCompleteModule } from "primeng/autocomplete";
 import { CalendarModule } from "primeng/calendar";
 import { ChipsModule } from "primeng/chips";
+import { ChipModule } from "primeng/chip";
 import { DropdownModule } from "primeng/dropdown";
 import { InputMaskModule } from "primeng/inputmask";
 import { InputNumberModule } from "primeng/inputnumber";
@@ -12,26 +13,51 @@ import { CascadeSelectModule } from "primeng/cascadeselect";
 import { MultiSelectModule } from "primeng/multiselect";
 import { InputTextareaModule } from "primeng/inputtextarea";
 import { InputTextModule } from "primeng/inputtext";
+import { RatingModule } from 'primeng/rating';
+import { KnobModule } from 'primeng/knob';
+import { ListboxModule } from 'primeng/listbox';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { SliderModule } from 'primeng/slider';
 import { CandidateComponent } from './candidate.component';
 
 
-
 @NgModule({
-  declarations: [CandidateComponent],
-  imports: [
-        CommonModule,
-        CandidateRoutingModule,
-        FormsModule,
+	declarations: [CandidateComponent],
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		CandidateRoutingModule,
+		FormsModule,
 		AutoCompleteModule,
 		CalendarModule,
 		ChipsModule,
 		DropdownModule,
 		InputMaskModule,
 		InputNumberModule,
+		ColorPickerModule,
 		CascadeSelectModule,
 		MultiSelectModule,
+		ToggleButtonModule,
+		SliderModule,
 		InputTextareaModule,
-		InputTextModule
-  ]
+		RadioButtonModule,
+		InputTextModule,
+		RatingModule,
+		ChipModule,
+		KnobModule,
+		InputSwitchModule,
+		ListboxModule,
+		SelectButtonModule,
+		CheckboxModule,
+		ButtonModule
+
+	]
 })
 export class CandidateModule { }
