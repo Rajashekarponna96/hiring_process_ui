d0aa269b19465b20ed717992c04ca60c8c1f2226
thanuja105 <v.thanuja105@gmail.com>
2024-03-15 11:54:28 +0530

    login page...

--- src/app/demo/components/dashboards/loginuser/loginuser.component.ts
+++ src/app/demo/components/dashboards/loginuser/loginuser.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { UserAccout } from '../../model/userAccount';
+import { Router } from '@angular/router';
 
 @Component({
-  //selector: 'app-loginuser',
+  selector: 'app-loginuser',
   templateUrl: './loginuser.component.html',
-  //styleUrls: ['./loginuser.component.css']
+  styleUrls: ['./loginuser.component.css']
 })
 export class LoginuserComponent implements OnInit {
 
+  userAccount = new UserAccout();
+  userAccounts:UserAccout[] | undefined
 
-  constructor(private layoutService: LayoutService) { }
+
+  constructor(private layoutService: LayoutService,private http: HttpClient, private changeDetectorRefs: ChangeDetectorRef,private router: Router) { }
 
   ngOnInit() {
+    this.getAllUserAccountList();
   }
 
   rememberMe: boolean = false;
@@ -20,4 +27,37 @@ export class LoginuserComponent implements OnInit {
     return this.layoutService.config.colorScheme !== 'light';
   }
 
-}
+  getUserAccountList() {
+    return this.http.get<UserAccout[]>('http://localhost:9000/userAccount/all');
+
+  }
+  getAllUserAccountList() {
+    return this.getUserAccountList().
+      subscribe((data) => {
+        console.log(data);
+        this.userAccounts = data;
+        this.changeDetectorRefs.markForCheck();
+      });
+  }
+
+  addLogin() {
+    this.http.post<UserAccout>('http://localhost:9000/userAccount/login', this.userAccount).subscribe(
+      res => {
+        console.log(res);
+        this.getAllUserAccountList();
+        this.router.navigate(['/home']);
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured.");
+        } else {
+          console.log("Server-side error occured."); 
+          this.router.navigate(['/loginuser']);
+        }
+
+      });
+    console.log(JSON.stringify(this.userAccount));
+    this.getAllUserAccountList();
+
+  }
+  }
