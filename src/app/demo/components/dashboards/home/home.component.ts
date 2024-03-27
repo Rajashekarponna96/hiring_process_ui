a156b3a04f4812ec0c47f45a61395bcbcac07e42
rajashekarponna96 <rajashekar.ponna96@gmail.com>
2024-03-27 12:20:48 +0530

    dashboard changes

--- src/app/demo/components/dashboards/home/home.component.ts
+++ src/app/demo/components/dashboards/home/home.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Job } from '../../model/job';
 
 @Component({
-  //selector: 'app-home',
+  selector: 'app-home',
   templateUrl: './home.component.html',
-  //styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
-  }
+export class HomeComponent   implements OnInit, OnDestroy {
+
+  jobs: any[] = []; // Assuming the API response will be an array of objects
+
+  constructor(private http: HttpClient) { }
+
   ngOnInit(): void {
-      throw new Error('Method not implemented.');
+    this.getAllJobs();
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  getAllJobs(): void {
+    this.http.get<any[]>('http://localhost:9000/job/summaries')
+      .subscribe(
+        (response) => {
+          // Check if response is an array
+          if (Array.isArray(response)) {
+            this.jobs = response;
+          } else {
+            console.error('Invalid API response: Expected an array but received:', response);
+          }
+        },
+        (error) => {
+          console.error('Error fetching jobs:', error);
+        }
+      );
   }
 
 }
+
