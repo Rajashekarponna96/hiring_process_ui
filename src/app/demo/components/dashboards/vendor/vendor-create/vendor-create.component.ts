fa6ce5b1823ced09b9cf345d69eb6278ff6a330d
rajashekarponna96 <rajashekar.ponna96@gmail.com>
2024-04-19 16:58:31 +0530

    errors in vendor

--- src/app/demo/components/dashboards/vendor/vendor-create/vendor-create.component.ts
+++ src/app/demo/components/dashboards/vendor/vendor-create/vendor-create.component.ts
@@ -1,10 +1,10 @@
 
-
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Vendor } from '../../../model/vendor'; // Import Vendor model
 import { VendorPoc } from '../../../model/vendorPoc'; // Import VendorPoc model
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-vendor-create',
@@ -27,21 +27,21 @@ export class VendorCreateComponent implements OnInit {
     }
   }
   addVendor() {
-        // Assign the POCs to the vendor
-        this.vendor.pocs = this.vendorPocs;
+    // Assign the POCs to the vendor
+    this.vendor.pocs = this.vendorPocs;
 
-        // Post the vendor data to the server
-        this.http.post<Vendor>('http://localhost:9000/vendor/', this.vendor).subscribe(
-          res => {
-            console.log(res);
-            // Navigate to the vendor page after adding
-            this.router.navigate(['/vendor-list']);
-          },
-          error => {
-            console.error('Error occurred while adding vendor:', error);
-          }
-        );
+    // Post the vendor data to the server
+    this.http.post<Vendor>('http://localhost:9000/vendor/', this.vendor).subscribe(
+      res => {
+        console.log(res);
+        // Navigate to the vendor page after adding
+        this.router.navigate(['/vendor-list']);
+      },
+      error => {
+        console.error('Error occurred while adding vendor:', error);
       }
+    );
+  }
   addPocField() {
     this.vendorPoc = new VendorPoc(); // Reset vendorPoc object
   }
@@ -106,9 +106,9 @@ export class VendorCreateComponent implements OnInit {
     const selectedPoc = this.vendorPocs[index];
 
     // Set the fields to be edited
-    this.vendorPoc.name =  selectedPoc.name;
-    this.vendorPoc.mobile =  selectedPoc.mobile;
-    this.vendorPoc.email =  selectedPoc.email;
+    this.vendorPoc.name = selectedPoc.name;
+    this.vendorPoc.mobile = selectedPoc.mobile;
+    this.vendorPoc.email = selectedPoc.email;
 
     // Set edit mode and selected index
     this.editMode = true;
@@ -118,239 +118,3 @@ export class VendorCreateComponent implements OnInit {
   ngOnInit() {
   }
 }
-
-// import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-// import { Vendor } from '../../../model/vendor'; // Import Vendor model
-// import { VendorPoc } from '../../../model/vendorPoc'; // Import VendorPoc model
-// import { Router } from '@angular/router';
-// import { HttpClient } from '@angular/common/http';
-
-// @Component({
-//   selector: 'app-vendor-create',
-//   templateUrl: './vendor-create.component.html',
-//   styleUrls: ['./vendor-create.component.css']
-// })
-// export class VendorCreateComponent implements OnInit {
-
-//   constructor(private router: Router, private http: HttpClient, private changeDetectorRefs: ChangeDetectorRef) { }
-
-//   showPocsFields: boolean = false;
-//   vendor: Vendor = new Vendor(); // Initialize vendor object
-//   vendorPoc: VendorPoc = new VendorPoc(); // Initialize vendorPoc object
-//   vendorPocs: VendorPoc[] = []; // Initialize vendorPocs array
-
-//   togglePocsFields() {
-//     this.showPocsFields = !this.showPocsFields;
-//     if (this.showPocsFields) {
-//       this.addPocField(); // Call function to add a new POC field
-//     }
-//   }
-
-//   addPocField() {
-//     this.vendorPoc = new VendorPoc(); // Reset vendorPoc object
-//   }
-
-//   deletePoc(i: any) {
-//     this.vendorPocs.splice(i, 1);
-//   }
-
-//   showPocsFields: boolean = false;
-//   client: Vendor = new Client(); // Initialize client object
-//   pointToContact: Poc = new Poc(); // Initialize pointToContact object
-//   pointToContacts: Poc[] = []; // Initialize pointToContacts array
-
-//   EditPoc(index: number) {
-//     const selectedPoc = this.pointToContacts[index];
-
-//     // Set the fields to be edited
-//     this.pointToContact.name =  selectedPoc.name;
-//     this.pointToContact.mobile =  selectedPoc.mobile;
-//     this.pointToContact.email =  selectedPoc.email;
-
-
-//     // Set edit mode and selected index
-//     this.editMode = true;
-//     this.selectedIndex = index;
-
-//   }
-//   submitPocs() {
-//     // Validate the POC details before adding or updating in the table
-//     if (this.validatePoc()) {
-//       // Check if the form is in edit mode
-//       if (this.editMode && this.selectedIndex !== undefined && this.selectedIndex !== null) {
-//         // Update the existing POC details
-//         this.vendorPocs[this.selectedIndex] = { ...this.vendorPoc };
-
-//         // Reset edit mode and selected index
-//         this.editMode = false;
-//         this.selectedIndex = null;
-//       } else {
-//         // Check if the POC already exists
-//         const existingIndex = this.vendorPocs.findIndex(poc => poc.name === this.vendorPoc.name && poc.mobile === this.vendorPoc.mobile);
-
-//         if (existingIndex !== -1) {
-//           // Update the existing POC details
-//           this.vendorPocs[existingIndex] = { ...this.vendorPoc };
-//         } else {
-//           // Add new POC details to the table
-//           this.vendorPocs.push({ ...this.vendorPoc });
-//         }
-//       }
-
-//       // Clear the form fields after submission
-//       this.clearPocFields();
-//     }
-//   }
-
-//   clearPocFields() {
-//     // Clear the form fields
-//     this.vendorPoc = {
-//       name: "",
-//       mobile: "",
-//       email: ""
-//     };
-
-//     // Hide the POC fields
-//     this.showPocsFields = false;
-//   }
-
-//   validatePoc(): boolean {
-//     // Validate that all fields are filled
-//     if (!this.vendorPoc.name || !this.vendorPoc.email || !this.vendorPoc.mobile) {
-//       throw new Error('All fields are required');
-//     }
-//     return true;
-//   }
-
-//   editMode: boolean = false; // Indicates whether the form is in edit mode
-//   selectedIndex: number | null = null; // Index of the currently selected row for editing
-
-//   addVendor() {
-//     // Assign the POCs to the vendor
-//     this.vendor.pocs = this.vendorPocs;
-
-//     // Post the vendor data to the server
-//     this.http.post<Vendor>('http://localhost:9000/vendor/', this.vendor).subscribe(
-//       res => {
-//         console.log(res);
-//         // Navigate to the vendor page after adding
-//         this.router.navigate(['/vendor']);
-//       },
-//       error => {
-//         console.error('Error occurred while adding vendor:', error);
-//       }
-//     );
-//   }
-
-//   ngOnInit() {
-//   }
-// }
-
-
-// import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-// import { Vendor } from '../../../model/vendor'; // Import Vendor model
-// import { VendorPoc } from '../../../model/vendorPoc'; // Import VendorPoc model
-// import { Router } from '@angular/router';
-// import { HttpClient } from '@angular/common/http';
-
-// @Component({
-//   selector: 'app-vendor-create',
-//   templateUrl: './vendor-create.component.html',
-//   styleUrls: ['./vendor-create.component.css']
-// })
-// export class VendorCreateComponent implements OnInit {
-
-//   constructor(private router: Router, private http: HttpClient, private changeDetectorRefs: ChangeDetectorRef) { }
-
-//   showPocsFields: boolean = false;
-//   vendor: Vendor = new Vendor(); // Initialize vendor object
-//   vendorPoc: VendorPoc = new VendorPoc(); // Initialize vendorPoc object
-//   vendorPocs: VendorPoc[] = []; // Initialize vendorPocs array
-
-//   togglePocsFields() {
-//     this.showPocsFields = !this.showPocsFields;
-//     if (this.showPocsFields) {
-//       this.addPocField(); // Call function to add a new POC field
-//     }
-//   }
-
-//   addPocField() {
-//     this.vendorPoc = new VendorPoc(); // Reset vendorPoc object
-//   }
-
-//   deletePoc(i: any) {
-//     this.vendorPocs.splice(i, 1);
-//   }
-
-//   submitPocs() {
-//     // Validate the POC details before adding or updating in the table
-//     if (this.validatePoc()) {
-//       // Check if the form is in edit mode
-//       if (this.editMode && this.selectedIndex !== undefined && this.selectedIndex !== null) {
-//         // Update the existing POC details
-//         this.vendorPocs[this.selectedIndex] = { ...this.vendorPoc };
-
-//         // Reset edit mode and selected index
-//         this.editMode = false;
-//         this.selectedIndex = null;
-//       } else {
-//         // Check if the POC already exists
-//         const existingIndex = this.vendorPocs.findIndex(poc => poc.name === this.vendorPoc.name && poc.mobile === this.vendorPoc.mobile);
-
-//         if (existingIndex !== -1) {
-//           // Update the existing POC details
-//           this.vendorPocs[existingIndex] = { ...this.vendorPoc };
-//         } else {
-//           // Add new POC details to the table
-//           this.vendorPocs.push({ ...this.vendorPoc });
-//         }
-//       }
-
-//       // Clear the form fields after submission
-//       this.clearPocFields();
-//     }
-//   }
-
-//   clearPocFields() {
-//     // Clear the form fields
-//     this.vendorPoc = {
-//       name: "",
-//       mobile: "",
-//       email: ""
-//     };
-
-//     // Hide the POC fields
-//     this.showPocsFields = false;
-//   }
-
-//   validatePoc(): boolean {
-//     // Validate that all fields are filled
-//     if (!this.vendorPoc.name || !this.vendorPoc.email || !this.vendorPoc.mobile) {
-//       throw new Error('All fields are required');
-//     }
-//     return true;
-//   }
-
-//   editMode: boolean = false; // Indicates whether the form is in edit mode
-//   selectedIndex: number | null = null; // Index of the currently selected row for editing
-
-//   addVendor() {
-//     // Assign the POCs to the vendor
-//     this.vendor.pocs = this.vendorPocs;
-
-//     // Post the vendor data to the server
-//     this.http.post<Vendor>('http://localhost:9000/vendor/', this.vendor).subscribe(
-//       res => {
-//         console.log(res);
-//         // Navigate to the vendor page after adding
-//         this.router.navigate(['/vendor']);
-//       },
-//       error => {
-//         console.error('Error occurred while adding vendor:', error);
-//       }
-//     );
-//   }
-
-//   ngOnInit() {
-//   }
-// }
