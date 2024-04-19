1cde3df447f6c392b662e682c2ee7634b4f1ca99
rajashekarponna96 <rajashekar.ponna96@gmail.com>
2024-04-19 13:06:01 +0530

    home compoent job cards changes

--- src/app/demo/components/dashboards/home/home.component.ts
+++ src/app/demo/components/dashboards/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { DataView } from 'primeng/dataview';
 import { Job } from '../../model/job';
 import { Router } from '@angular/router';
 import { SelectItem } from 'primeng/api';
@@ -14,7 +15,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   jobs: any[] = []; // Assuming the API response will be an array of objects
   clientNames: { [clientId: string]: string } = {}; // Object to store client names by ID
-  constructor(private http: HttpClient,private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.getAllJobsWithClients();
@@ -43,18 +44,42 @@ export class HomeComponent implements OnInit, OnDestroy {
   //
 
 
-handleViewJob(job: Job) {
-  console.log('Job object to view:', job); // Log the job object
-  // Navigate to the 'viewjob' route with the job object as a parameter in the state
-  this.router.navigate(['jobview'], { state: { job: job } });
-}
+  handleViewJob(job: Job) {
+    console.log('Job object to view:', job); // Log the job object
+    // Navigate to the 'viewjob' route with the job object as a parameter in the state
+    this.router.navigate(['jobview'], { state: { job: job } });
+  }
 
 
-sortField: string = '';
-sortOptions: SelectItem[] = [
-  { label: 'Recent Posts', value: 'share' },
-  { label: 'Old Posts', value: 'comment' }
-];
+  sortField: string = '';
+  sortOptions: SelectItem[] = [
+    { label: 'Recent Posts', value: 'share' },
+    { label: 'Old Posts', value: 'comment' }
+  ];
+
+
+  //
+  sortOrder: number = 0;
+  onSortChange(event: any) {
+    const value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
+  }
+
+
+  onFilter(dv: DataView, event: Event) {
+    dv.filter((event.target as HTMLInputElement).value);
+  }
+
+
+  //
+
 
 }
 
