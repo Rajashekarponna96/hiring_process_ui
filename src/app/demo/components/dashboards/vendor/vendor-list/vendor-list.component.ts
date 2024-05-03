a298f344ed1484d35753aa9b05afb186c23f99e5
shaliniEdara <edara.shalini@gmail.com>
2024-05-03 11:51:48 +0530

    final pagination code in vendor controller

--- src/app/demo/components/dashboards/vendor/vendor-list/vendor-list.component.ts
+++ src/app/demo/components/dashboards/vendor/vendor-list/vendor-list.component.ts
@@ -4,11 +4,7 @@ import { Vendor } from '../../../model/vendor';
 import { MessageService, ConfirmationService } from 'primeng/api';
 import { Router } from '@angular/router';
 import { NodeService } from 'src/app/demo/service/node.service';
-import { Table } from 'primeng/table';
-
-interface ExpandedRows {
-  [key: string]: boolean;
-}
+import { Pagination } from '../../../model/pagination';
 
 @Component({
   selector: 'app-vendor-list',
@@ -18,8 +14,11 @@ interface ExpandedRows {
 })
 export class VendorListComponent implements OnInit {
   vendors: Vendor[] = [];
-  expandedRows: ExpandedRows = {};
-  isExpanded = false;
+  pagination!: Pagination;
+  totalElements: number = 0;
+  totalPages: number = 0;
+  currentPage: number = 0;
+  selectedRecordsOption1: number = 5;
 
   constructor(
     private router: Router,
@@ -33,65 +32,28 @@ export class VendorListComponent implements OnInit {
   }
 
   getAllVendorList() {
-    this.http.get<Vendor[]>('http://localhost:9000/vendor/all')
-      .subscribe((data) => {
-        this.vendors = data;
-      });
-  }
+    this.http.get<any>('http://localhost:9000/vendor/vendorlistwithpagination', {
 
-  expandAll() {
-    if (!this.isExpanded) {
-      this.vendors.forEach(vendor => {
-        if (vendor && vendor.id) {
-          this.expandedRows[vendor.id] = true;
-        }
-      });
-    } else {
-      this.expandedRows = {};
-    }
-    this.isExpanded = !this.isExpanded;
+      params: {
+        page: this.currentPage.toString(),
+        size: this.selectedRecordsOption1.toString()
+      }
+    }).subscribe((data) => {
+      this.vendors = data.content;
+      this.pagination = data;
+      this.totalElements = data.totalElements;
+      this.totalPages = data.totalPages;
+      this.changeDetectorRefs.markForCheck();
+    });
   }
 
   navigateToCreateVendor() {
-    this.router.navigate(['vendor-create']);
-  }
-  navigateToCreateVendor1() {
     this.router.navigate(['vendor-create']);
   }
 
   handleEditVendor(vendor: Vendor, vendorId: number) {
     this.router.navigate(['vendor-edit'], { state: { vendorId: vendorId, vendor: vendor } });
   }
-
-  onGlobalFilter(table: Table, event: Event) {
-    table.filterGlobal(
-        (event.target as HTMLInputElement).value,
-        'contains'
-    );
-}
-
-onGlobalFilter1(event: Event) {
-  const inputElement = event.target as HTMLInputElement;
-  const inputValue = inputElement.value;
-  console.log('Input Value:', inputValue);
-  this.http.get<any>('http://localhost:9000/vendor/searchpage', {
-      params: {
-          // firstName: inputValue,
-          // lastName:inputValue,
-          // email: inputValue
-          code:inputValue,
-          page: 0,
-          size: 3
-
-      }
-  }).subscribe((data) => {
-
-      this.vendors = data["content"]
-       this.changeDetectorRefs.markForCheck();
-  });
-
-}
-
 
   vendorDelete(vendor: Vendor) {
     console.log("vendor id is:" + vendor.id);
@@ -111,9 +73,76 @@ onGlobalFilter1(event: Event) {
       );
   }
 
-  goToFirstPage(){}
-  goToPreviousPage(){}
-  goToNextPage(){}
-  goToLastPage(){}
-}
+  goToFirstPage() {
+    this.currentPage = 0;
+    this.getAllVendorList();
+  }
 
+  goToPreviousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.getAllVendorList();
+    }
+  }
+
+  goToNextPage() {
+    if (this.currentPage < this.totalPages - 1) {
+      this.currentPage++;
+      this.getAllVendorList();
+    }
+  }
+
+  goToLastPage() {
+    this.currentPage = this.totalPages - 1;
+    this.getAllVendorList();
+  }
+
+  onRecordsPerPageChange(event: Event) {
+    this.selectedRecordsOption1 = +(event.target as HTMLSelectElement).value;
+    this.currentPage = 0; // Reset to first page when changing page size
+    this.getAllVendorList();
+  }
+
+  // onGlobalFilter1(event: Event) {
+  //   const inputElement = event.target as HTMLInputElement;
+  //   const inputValue = inputElement.value;
+  //   console.log('Input Value:', inputValue);
+  //   this.http.get<any>('http://localhost:9000/vendor/searchpage', {
+  //       params: {
+  //           // firstName: inputValue,
+  //           // lastName:inputValue,
+  //           // email: inputValue
+  //           code:inputValue,
+  //           page:this.currentPage.toString(),
+  //           size: this.selectedRecordsOption1
+  
+  //       }
+  //   }).subscribe((data) => {
+       
+  //       this.vendors = data["content"]
+        
+  //        this.changeDetectorRefs.markForCheck();
+  //   });
+  
+  // }
+
+
+  onGlobalFilter1(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const inputValue = inputElement.value;
+    console.log('Input Value:', inputValue);
+    this.http.get<any>('http://localhost:9000/vendor/searchpage', {
+      params: {
+        code: inputValue,
+        page: '0', // Reset to first page when applying filter
+        size: this.selectedRecordsOption1.toString()
+      }
+    }).subscribe((data) => {
+      this.vendors = data["content"];
+      this.totalElements = data.totalElements;
+      this.totalPages = data.totalPages;
+      this.currentPage = 0; // Reset to first page
+      this.changeDetectorRefs.markForCheck();
+    });
+  }
+}
