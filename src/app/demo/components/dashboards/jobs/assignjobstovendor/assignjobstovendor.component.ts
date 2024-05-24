50366203ad9033c1c2f498f34febae841567303d
rajashekarponna96 <rajashekar.ponna96@gmail.com>
2024-05-24 10:29:43 +0530

    assignjobstovendotapis

--- src/app/demo/components/dashboards/jobs/assignjobstovendor/assignjobstovendor.component.ts
+++ src/app/demo/components/dashboards/jobs/assignjobstovendor/assignjobstovendor.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Job } from '../../../model/job';
+import { Pagination } from '../../../model/pagination';
+import { Vendor } from '../../../model/vendor';
+import { JobService } from '../job.service';
 
 @Component({
   selector: 'app-assignjobstovendor',
@@ -6,10 +11,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignjobstovendor.component.css']
 })
 export class AssignjobstovendorComponent implements OnInit {
+  job: Job = new Job();
+  jobs: Job[] = [];
+  vendors: Vendor[] = []; // List of vendors
+  pagination!: Pagination;
+  totalElements: number = 0;
+  totalPages: number = 0;
+  currentPage: number = 0;
+  selectedRecordsOption1: number = 5;
+  selectedVendorId: number | null = null; // Track selected vendor for assignment
+  selectedJobId: number | null = null; // Track selected job for assignment
 
-  constructor() { }
+  constructor(private jobService: JobService, private changeDetectorRefs: ChangeDetectorRef, private router: Router) { }
 
   ngOnInit() {
+    this.getAllJobs();
+    this.getAllVendors(); // Fetch vendors on init
   }
 
+  getAllJobs() {
+    this.jobService.getAllJobs().subscribe((data) => {
+      this.jobs = data;
+      this.changeDetectorRefs.markForCheck();
+    });
+  }
+
+  getAllVendors() {
+    this.jobService.getAllVendors().subscribe((data) => {
+      this.vendors = data;
+      this.changeDetectorRefs.markForCheck();
+    });
+  }
+
+  assignJobsToVendor() {
+    if (this.selectedJobId && this.selectedVendorId) {
+      this.jobService.assignVendorToJob(this.selectedJobId, this.selectedVendorId).subscribe(() => {
+        console.log(`Assigned job ${this.selectedJobId} to vendor ${this.selectedVendorId}`);
+        // Reset the form fields
+        this.selectedJobId = null;
+        this.selectedVendorId = null;
+        // Optionally, reload the data after assigning the job to the vendor
+        this.getAllJobs();
+      });
+    } else {
+      console.warn('Both job and vendor must be selected for assignment.');
+    }
+  }
+
+
+
 }
+
+
---
+++ src/app/demo/components/dashboards/jobs/job.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Job } from '../../model/job';
+import { Vendor } from '../../model/vendor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+  private baseUrl = 'http://localhost:9000/job';
+
+  constructor(private http: HttpClient) {}
+
+  getAllJobs(): Observable<Job[]> {
+    return this.http.get<Job[]>(`${this.baseUrl}/alljobslist`);
+  }
+
+  getAllVendors(): Observable<Vendor[]> {
+    return this.http.get<Vendor[]>(`${this.baseUrl}/allvendorslist`);
+  }
+
+  assignVendorToJob(jobId: number, vendorId: number): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/${jobId}/vendors`, { vendorId });
+  }
+
+  getJobList(page: number, size: number): Observable<any> {
+    let params = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    return this.http.get<any>(`${this.baseUrl}/joblistwithpagination`, { params });
+  }
+
+  searchJobs(code: string, page: number, size: number): Observable<any> {
+    let params = new HttpParams().set('code', code).set('page', page.toString()).set('size', size.toString());
+    return this.http.get<any>(`${this.baseUrl}/searchpage`, { params });
+  }
+
+  deleteJob(jobId: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${jobId}`);
+  }
+}
