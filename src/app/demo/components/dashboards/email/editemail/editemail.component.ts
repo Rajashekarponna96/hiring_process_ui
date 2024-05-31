a02e3754aaa54dcbad520e7dbe5faa2d686df771
rajashekarponna96 <rajashekar.ponna96@gmail.com>
2024-05-31 15:35:37 +0530

    EmailtemplateService

--- src/app/demo/components/dashboards/email/editemail/editemail.component.ts
+++ src/app/demo/components/dashboards/email/editemail/editemail.component.ts
@@ -1,57 +1,43 @@
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { CandidateEmail } from '../../../model/candidateEmail';
 import { NgForm } from '@angular/forms';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { EmailtemplateService } from 'src/app/demo/service/emailtemplateervice.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-editemail',
   templateUrl: './editemail.component.html',
   styleUrls: ['./editemail.component.scss']
 })
-export class EditemailComponent {
+export class EditemailComponent implements OnInit {
 
   candidateEmail: CandidateEmail = new CandidateEmail();
-  candidateEmails: CandidateEmail[] | undefined
+  candidateEmails: CandidateEmail[] = [];
 
   @ViewChild("jobForm")
   emailForm!: NgForm;
 
   titles: any[] = [
-    {
-      "id": 1,
-      "name": 'Sourced'
-    },
-    {
-      "id": 2,
-      "name": 'Screening'
-    },
-    {
-      "id": 3,
-      "name": 'Interview'
-    },
-    {
-      "id": 4,
-      "name": 'Preboarding'
-    },
-    {
-      "id": 1,
-      "name": 'Hired'
-    },
-    {
-      "id": 1,
-      "name": 'Archived'
-    },
-  ]
+    { "id": 1, "name": 'Sourced' },
+    { "id": 2, "name": 'Screening' },
+    { "id": 3, "name": 'Interview' },
+    { "id": 4, "name": 'Preboarding' },
+    { "id": 5, "name": 'Hired' },
+    { "id": 6, "name": 'Archived' },
+  ];
 
-  
-  constructor(private http: HttpClient, private changeDetectorRefs: ChangeDetectorRef, private router: Router) { }
+  constructor(
+    private emailService: EmailtemplateService,
+    private changeDetectorRefs: ChangeDetectorRef,
+    private router: Router
+  ) { }
+
   ngOnInit() {
-    const stateData = history.state || {}; 
+    const stateData = history.state || {};
     const candidateEmail = stateData.candidateEmail;
     if (candidateEmail) {
       this.candidateEmail = candidateEmail;
-      
     } else {
       console.error('candidateEmail data is missing in state.');
     }
@@ -59,27 +45,23 @@ export class EditemailComponent {
     this.getAllEmailList();
   }
 
-  getEmailList() {
-    return this.http.get<CandidateEmail[]>(
-        'http://localhost:9000/email/all'
-    );
-}
-getAllEmailList() {
-    return this.getEmailList().subscribe((data) => {
+  getAllEmailList() {
+    this.emailService.getEmailList().subscribe(
+      (data) => {
         console.log(data);
         this.candidateEmails = data;
         this.changeDetectorRefs.markForCheck();
-    });
-}
+      },
+      (err: HttpErrorResponse) => {
+        console.error('Error fetching email list:', err);
+      }
+    );
+  }
 
   updateEmail() {
-    this.http.put<CandidateEmail>('http://localhost:9000/email/' + this.candidateEmail.id, this.candidateEmail).subscribe(
+    this.emailService.updateEmail(this.candidateEmail).subscribe(
       res => {
         console.log(res);
-        // this.getAllEmailList();
-        // this.emailForm.reset();
-
-        // Redirect to the "/jobs" route after successfully updating the job
         this.router.navigateByUrl('/email');
       },
       (err: HttpErrorResponse) => {
@@ -91,6 +73,5 @@ getAllEmailList() {
       }
     );
   }
-
-
 }
+
